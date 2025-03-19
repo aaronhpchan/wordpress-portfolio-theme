d391ee986abba1467c64a7c5760c63f84605eea7
aaronhpchan <aaronhpchan@gmail.com>
2025-03-19 15:35:22 -0400

    Add animation effects

--- themes/portfolio-theme/index.php
+++ themes/portfolio-theme/index.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 
 <div class="banner" id="banner">
-  <div class="banner-container">
+  <div class="banner-container fade-in">
     <div>
       <div class="banner-container__text">
         <h1><span class="banner-intro">Hello, I am</span><?php echo get_bloginfo('name'); ?></h1>
-        <p class="banner-role"><span class="banner-role__text">Web Developer</span></p>
+        <p class="banner-role"><span class="banner-role__text"></span></p>
       </div>
       <div class="banner-links__wrapper">
         <a href="#contact">
@@ -14,7 +14,7 @@
       </div>
     </div>
   </div>
-  <div class="banner-container">
+  <div class="banner-container fade-in">
     <img src="<?php echo get_theme_file_uri('/images/header_img.png'); ?>">
   </div>
 </div>
@@ -52,10 +52,10 @@
       $about->the_post(); ?>
       <h2><?php the_title(); ?></h2>
       <div class="about-container">
-        <div class="about-img">
+        <div class="about-img fade-in">
           <img src="<?php echo the_post_thumbnail_url(); ?>">
         </div>
-        <div class="about-info">
+        <div class="about-info fade-in">
           <div class="about-info__text"><?php the_content(); ?></div>
           <div class="about-info__container">
             <div>
@@ -89,12 +89,12 @@
       for ($i = 0; $i < $projectNum; $i++) {
         $projects->the_post(); ?>
         <div class="projects-container">
-          <div class="projects-item" <?php if ($i % 2 == 0) echo 'style="order: 1;"' ?>>
+          <div class="projects-item fade-in slide-in <?php if ($i % 2 == 0) echo 'projects-item__right animation-right'; if ($i % 2 !== 0) echo 'animation-left'; ?>">
             <a href="<?php the_field('project_link'); ?>" target="_blank">
               <img src="<?php echo the_post_thumbnail_url(); ?>">
             </a>
           </div>
-          <div class="projects-item">
+          <div class="projects-item fade-in slide-in <?php if ($i % 2 == 0) echo 'animation-left'; if ($i % 2 !== 0) echo 'animation-right'; ?>">
             <div class="projects-item__info">
               <h3><a href="<?php the_field('project_link'); ?>" target="_blank"><?php the_title(); ?></a></h3>
               <div>
@@ -123,10 +123,10 @@
 <div class="contact" id="contact">
   <h2>Contact Me</h2>
   <div class="contact-container">
-    <div class="contact-map">
+    <div class="contact-map fade-in">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92025.38686629602!2d-79.3819386976085!3d43.88083323127541!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4d5efa0324ca9%3A0xf73d52812cb23d63!2sMarkham%2C%20ON!5e0!3m2!1sen!2sca!4v1742154520650!5m2!1sen!2sca" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
-    <div class="contact-form">
+    <div class="contact-form fade-in">
       <?php echo do_shortcode('[wpforms id="77"]'); ?>
     </div>  
   </div>
--- themes/portfolio-theme/src/index.js
+++ themes/portfolio-theme/src/index.js
@@ -52,3 +52,35 @@ class TextAnimation {
   }
 }
 const textAnimation = new TextAnimation();
+
+/* Intersection observer animation */
+class ObserverAnimation {
+  constructor() {
+    const faders = document.querySelectorAll(".fade-in");
+    const sliders = document.querySelectorAll(".slide-in");
+
+    const appearOptions = {
+      threshold: 0,
+      rootMargin: "0px 0px -250px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          entry.target.classList.add("appear");
+          appearOnScroll.unobserve(entry.target);
+        }
+      })
+    }, appearOptions);
+
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+    });
+    sliders.forEach(slider => {
+      appearOnScroll.observe(slider);
+    });
+  }
+}
+const observerAnimation = new ObserverAnimation();
